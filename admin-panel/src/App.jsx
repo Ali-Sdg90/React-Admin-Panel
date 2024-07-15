6eac1d490426ab764fe8e7131aaca358cf044922
Ali Sadeghi <ali909392@gmail.com>
2024-07-15 13:12:13 +0330

    Add base file and folders

---
+++ admin-panel/src/App.jsx
@@ -0,0 +1,11 @@
+import Style from "./assets/styles/App.module.scss";
+
+function App() {
+    return (
+        <div className={Style.container}>
+            <h1>Aloha</h1>
+        </div>
+    );
+}
+
+export default App;
